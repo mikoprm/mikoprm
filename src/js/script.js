a37dedef38c9ec5cc74dc0672bc387a0d9ddc2f7
Mikoprn <b4gas24@gmail.com>
2025-05-31 19:54:55 +0700

    move the script

--- src/js/script.js
+++ src/js/script.js
@@ -1,61 +1,51 @@
-// Header on scroll
-const header = document.querySelector("header");
-let lastScrollY = window.scrollY;
+  // Header on scroll
+  const header = document.querySelector("header");
+  let lastScrollY = window.scrollY;
 
-window.addEventListener("scroll", () => {
-  if (lastScrollY < window.scrollY) {
-    header.classList.add("header-hidden");
-    // header.classList.remove("header-scrolled");
-  } else {
-    header.classList.remove("header-hidden");
-    // header.classList.add("header-scrolled");
-  }
-  lastScrollY = window.scrollY;
-});
-
-// Active Link Navbar In Current Page
-const navLinks = document.querySelectorAll("[data-navLink]");
-
-navLinks.forEach((link) => {
-    if (link.getAttribute("href") === window.location.pathname) {
-        link.setAttribute("aria-current", "page");
+  window.addEventListener("scroll", () => {
+    if (lastScrollY < window.scrollY) {
+      header.classList.add("header-hidden");
+      // header.classList.remove("header-scrolled");
+    } else {
+      header.classList.remove("header-hidden");
+      // header.classList.add("header-scrolled");
     }
-});
-
-// Mobile Navigation
-const menuBtn = document.querySelector(".nav__mobile-btn");
-const navMenu = document.querySelector(".nav__menu");
-// const navLinks = document.querySelectorAll("nav__link");
-
-menuBtn.addEventListener("click", () => {
-  navMenu.classList.toggle("active");
-});
-
-navMenu.addEventListener("click", () => {
-  navMenu.classList.remove("active");
-});
-
-document.addEventListener("click", (e) => {
-  if (!menuBtn.contains(e.target) && !navMenu.contains(e.target)) {
-    menuBtn.classList.remove("active");
-    navMenu.classList.remove("active");
-  }
-});
-
-// Project Tabs
-const tabs = document.querySelectorAll("[data-tab-target]");
-const tabContents = document.querySelectorAll("[data-tab-content]");
-
-tabs.forEach((tab) => {
-  tab.addEventListener("click", () => {
-    const target = document.querySelector(tab.dataset.tabTarget);
-    tabContents.forEach((tabContent) => {
-      tabContent.classList.remove("active");
-    });
-    tabs.forEach((tab) => {
-      tab.classList.remove("active");
-    });
-    tab.classList.add("active");
-    target.classList.add("active");
+    lastScrollY = window.scrollY;
   });
-});
+
+  // Mobile Navigation
+  const menuBtn = document.querySelector(".nav__mobile-btn");
+  const navMenu = document.querySelector(".nav__menu");
+
+  menuBtn.addEventListener("click", () => {
+    navMenu.classList.toggle("active");
+  });
+
+  navMenu.addEventListener("click", () => {
+    navMenu.classList.remove("active");
+  });
+
+  document.addEventListener("click", (e) => {
+    if (!menuBtn.contains(e.target) && !navMenu.contains(e.target)) {
+      menuBtn.classList.remove("active");
+      navMenu.classList.remove("active");
+    }
+  });
+
+  // Project Tabs
+  const tabs = document.querySelectorAll("[data-tab-target]");
+  const tabContents = document.querySelectorAll("[data-tab-content]");
+
+  tabs.forEach((tab) => {
+    tab.addEventListener("click", () => {
+      const target = document.querySelector(tab.dataset.tabTarget);
+      tabContents.forEach((tabContent) => {
+        tabContent.classList.remove("active");
+      });
+      tabs.forEach((tab) => {
+        tab.classList.remove("active");
+      });
+      tab.classList.add("active");
+      target.classList.add("active");
+    });
+  });
